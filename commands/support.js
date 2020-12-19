6c28f1a21f4d3fc85f3a36e435bcedf176517339
torohangupta <75713762+torohangupta@users.noreply.github.com>
2020-12-19 15:44:28 -0600

    created transcript logging system for support

--- commands/support.js
+++ commands/support.js
@@ -23,6 +23,8 @@ module.exports = {
             return;
         }
 
+        transcriptLogChannel = `789907442582028308`;
+
         // get author's username/nickname if it exists
         authorName = message.member.nickname;
         if (!authorName) authorName = message.author.username;
@@ -67,6 +69,15 @@ module.exports = {
                 sc.setName(`🟠-support-${message.author.username}`)
             })
 
+            // create message logging
+            msgLog = [];
+            logIndex = 0;
+            const msgLoggingCollector = sc.createMessageCollector(m => m.author.id != userIDs.walle);
+            msgLoggingCollector.on(`collect`, c => {
+                msgLog[logIndex] = `**${c.author.username}** - ${c.content}`;
+                logIndex++
+            })
+
             // create support embed
             const supportEmbed = new MessageEmbed()
                 .setTitle(`New support ticket - ${message.author.username}`)
@@ -90,10 +101,29 @@ module.exports = {
 
                 // delete support ticket channel after 2 valid reactions
                 deleteCollector.on('end', collected => {
+
+                    // stop all message collectors
+                    msgLoggingCollector.stop();
+                    scMsgCollectorHelping.stop();
+
+                    // create transcript embed
+                    const transcriptEmbed = new MessageEmbed()
+                    .setAuthor(`Wall-E Support`, `https://unitedtheme.com/live-preview/starter-gazette/wp-content/uploads/2018/12/image-005-720x720.jpg`)
+                        .setTitle(`Support Ticket Transcript - ${message.author.username}`)
+                        .setColor(`FF5733`)
+                        .setDescription(msgLog.join(`\n`))
+                        .setTimestamp()
+                        .setFooter(`Ticket opened by: ${authorName}`, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+                    
+                    // send a copy of the transcript embed to the user and another to the log-support channel
+                    message.author.send(`Here is a transcript of your support ticket:`)
+                    message.author.send(transcriptEmbed)
+                    message.client.channels.cache.get(transcriptLogChannel).send(transcriptEmbed)
+
                     sc.delete()
                         .then(msg => {
-                            console.log(`Deleted help embed, requested by \`${authorName}\``)
-                            message.client.channels.cache.get(consoleChannel).send(`Deleted help embed, requested by \`${authorName}\``);
+                            console.log(`Deleted \`${authorName}\`'s support ticket channel.`)
+                            message.client.channels.cache.get(consoleChannel).send(`Deleted \`${authorName}\`'s support ticket channel.`);
                         })
                         .catch(console.error);
                 });
