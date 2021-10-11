12758c8a4f7e20a104c010f2e449674a4d1934cc
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2021-10-11 00:22:14 -0500

    clean up

--- interactions/slashCommands/ping.js
+++ interactions/slashCommands/ping.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require(`discord.js`);
-const { consoleChannel, updatesChannel } = require(`../../dependencies/resources/config.json`);
 
 module.exports = {
 
@@ -9,17 +8,10 @@ module.exports = {
 
     execute(interaction) {
 
-        // if the command is in #class-commands, return eith error
-        if (interaction.channel.id == `789256304844603494`) {
-            return interaction.reply({ content: `You can't use that command here!`, ephemeral: true });
-        }
-
-
         const guildMemberObject = interaction.guild.members.cache.get(interaction.member.id);
         var userName = guildMemberObject.nickname ? guildMemberObject.nickname : guildMemberObject.user.username;
 
         const pingEmbed = new MessageEmbed()
-            .setAuthor('Wall-E Ping Command', 'https://unitedtheme.com/live-preview/starter-gazette/wp-content/uploads/2018/12/image-005-720x720.jpg')
             .setTitle(`🏓 Pong!`)
             .setColor(`ABD1C9`)
             .setDescription(`Client Ping: ${Date.now() - interaction.createdTimestamp}ms.\nAPI Latency: ${interaction.client.ws.ping}ms`)
