9b3be08a067a05ed3ed0f98f1eb19fe14b744ec9
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2022-09-24 13:49:45 -0500

    populated config file

--- core/config.js
+++ core/config.js
@@ -1,3 +1,41 @@
 module.exports = {
-    // initalize config file
-}
+    OWNER_ID: `382893405178691584`,
+    PREFIX: `~`, /** @deprecated */
+
+    PRESENCE: {
+        STATUS: `online`,
+        TYPE: `WATCHING`,
+        MESSAGE: `rohan write bad code`,
+    },
+
+    IMAGE: {
+        SQUARE: `https://unitedtheme.com/live-preview/starter-gazette/wp-content/uploads/2018/12/image-005-720x720.jpg`,
+    },
+    
+    /* Bot Embed Colors */
+    EMBED_COLORS: {
+        BOT_EMBED: `#068ADD`,
+        TRANSPARENT: `#36393F`,
+        SUCCESS: `#00A56A`,
+        ERROR: `#D61A3C`,
+        WARNING: `#F7E919`,
+        AUTOMOD: `#36393F`,
+        TICKET_CREATE: `#068ADD`,
+        TICKET_CLOSE: `#068ADD`,
+        KICK_LOG: `#FF7961`,
+        SOFTBAN_LOG: `#AF4448`,
+        BAN_LOG: `#D32F2F`,
+    },
+    
+    MESSAGES: {
+        API_ERROR: `Unexpected Backend Error! Try again later or contact support server`,
+    },
+
+    CHANNELS: {
+        CONSOLE: `768856507903115294`,
+        UPDATES: `801893303444045854`,
+        LOG_DMS: `787830182781255681`,
+        LOG_SUPPORT: `789907442582028308`,
+        LOG_MODAPPS: `933239367818944592`
+    }
+};
