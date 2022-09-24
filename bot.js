29b894f9519366c467a3d3b848f0596964f73c37
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2022-09-24 13:48:30 -0500

    removed asynchronous execution

--- bot.js
+++ bot.js
@@ -1,17 +1,15 @@
 require("dotenv").config();
 
-const { BotClient } = require(`./core/botClient`);
+const BotClient = require(`./core/botClient.js`);
 
 // initialize client
 const client = new BotClient();
 
-async () => {
-    await client.registerSlashCommands(/* dir */)
-    await client.loadCommands(/* dir */);
-    await client.loadButtonManagers(/* dir */);
-    await client.loadSelectMenus(/* dir */);
-    client.loadEvents(/* dir */);
-};
 
+// client.registerSlashCommands(/* dir */)
+// client.loadCommands(/* dir */);
+// client.loadButtonManagers(/* dir */);
+// client.loadSelectMenus(/* dir */);
+client.loadEvents(`./events`);
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.TOKEN);
