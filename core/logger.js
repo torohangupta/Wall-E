54c3ecb1d71db3886a619fd3d4fa075dd5e1219b
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2022-09-24 13:50:17 -0500

    Created logging file & functions

---
+++ core/logger.js
@@ -0,0 +1,26 @@
+const { chalk } = import(`chalk`);
+
+const BotClient = require(`./botClient.js`);
+
+
+/* Logging Functions
+    - Info/Debug (Blue)
+    - Warn (Yellow)
+    - Error (Red)
+*/
+
+function logInfo(params) {
+    
+}
+
+function logWarning(params) {
+
+}
+
+function logError(params) {
+
+}
+
+module.exports = {
+
+}
