5260333fa7724233782f94e41d99d845acb58dd2
Patrick Demers <patrickdemers6@gmail.com>
2022-09-28 11:03:31 -0500

    specify ISU professors specifically in all text

--- interactions/commands/ratemyprofessor.js
+++ interactions/commands/ratemyprofessor.js
@@ -13,16 +13,16 @@ module.exports = {
         const professorName = getProfessorName(interaction);
 
         if (!professorName)
-            return reply("Please specify a professor's name.");
+            return reply("Please specify an ISU professor's name.");
 
         const professors = await getProfessorsByName(professorName);
 
         if (professors.length == 0)
-            return reply('No professors found.');
+            return reply('No ISU professors found.');
         
         if (professors.length > 1) {
             const professorsFound = professors.map(prof => `⦁ ${prof.firstName} ${prof.lastName}\n`).join('');
-            return reply(`${professors.length} professors found. Please refine your query.\n\nNames found:\n${professorsFound}`);
+            return reply(`${professors.length} ISU professors found. Please refine your query.\n\nNames found:\n${professorsFound}`);
         }
 
         const professorDetails = await getProfessorDetails(professors[0]);
