ebcef0df23fc01aa5278038e452443266a36e84e
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2022-09-29 15:59:29 -0500

    Initialized Logger & attached to BotClient instance

--- bot.js
+++ bot.js
@@ -1,16 +1,23 @@
 require("dotenv").config();
 
 const BotClient = require(`./core/botClient.js`);
+const Logger = require(`./core/logger.js`)
 
 console.clear();
 
 // initialize client
 const client = new BotClient(process.env.ENVIRONMENT);
 
+// initialize logger & attach to new BotClient
+const logger = new Logger();
+client.logger = logger;
+
+client.logger.console(`DEBUG`, `Starting...`, `Initalized BotClient & attached Logger to the BotClient instance`);
 
 client.loadSlashCommands(`./interactions/commands`);
-// client.loadButtonManagers(/* dir */);
+client.loadButtons(`./interactions/buttons`);
 // client.loadSelectMenus(/* dir */);
 client.loadEvents(`./events`);
 
 client.login(process.env.TOKEN);
+client.logger.console(`DEBUG`, `Logging in with bot token...`);
