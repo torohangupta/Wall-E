0cbe4efbfb815cd5c41086d253d1d8f9a100dc1a
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2022-11-21 10:06:18 -0600

    Complete rewrite to handle `voiceStateUpdate` event

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -4,6 +4,57 @@ module.exports = {
 	once: false,
 
 	async execute(client, oldState, newState) {
-		// TODO: Rewrite
+		// get new voice button from config
+		const newVoiceChannelButton = client.config.NEW_VOICE_CHANNEL_BUTTON;
+
+		// determine if the oldState was a channel to be deleted
+		let emptyChannelCheck = oldState.channelId === null ? undefined :
+			oldState.channel.name.includes(`🔊│Comm`) && !oldState.channel.members.size;
+
+		// if oldState has no users remaining, remove it
+		if (emptyChannelCheck) await voiceDelete(client, oldState);
+
+		// user requests a new voice channel
+		if (newState.channelId === newVoiceChannelButton) return await voiceCreate(client, newState);
 	}
 };
+
+/**
+ * Function to create a new voice channel
+ * @param {Client} client active BotClient instance
+ * @param {Object} voiceState voiceState object
+ */
+async function voiceCreate(client, voiceState) {
+
+	// get array of voiceChannels
+	const voiceChannelArray = voiceState.guild.channels.cache
+		.filter(c => c.name.includes(`🔊│Comm`))
+		.map(c => c.name);
+
+	for (let i = 1; i < voiceChannelArray.length + 2; i++) {
+		if (voiceChannelArray.includes(`🔊│Comm ` + i)) continue;
+
+		const newVoiceChannel = await voiceState.guild.channels.create(`🔊│Comm ${i}`, {
+			type: 'GUILD_VOICE',
+			parent: voiceState.channel.parentId,
+			position: voiceState.channel.rawPosition + i,
+			bitrate: 64000
+		});
+
+		await voiceState.channel.members.map(m => m.voice.setChannel(newVoiceChannel.id));
+		// console.log(newVoiceChannel)
+		await client.logger.console(`INFO`, `Event - voiceStateUpdate`, `${newVoiceChannel.name} was created`);
+		return;
+	}
+}
+
+/**
+ * Function to remove a voice channel
+ * @param {Client} client active BotClient instance
+ * @param {Object} voiceChannel voiceState object
+ */
+async function voiceDelete(client, voiceChannel) {
+	await client.logger.console(`INFO`, `Event - voiceStateUpdate`, `${voiceChannel.channel.name} was removed`);
+	await voiceChannel.channel.delete();
+	return;
+}
