e1cbdc22fa96655c4295406b15cebc578ca8166c
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2022-11-21 10:07:54 -0600

    Created the `about` function
    Info about Wall-E & it's role in Online College

---
+++ interactions/commands/about.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    name: `about`,
+
+    execute(client, interaction) {
+        const config = client.config;
+        const embed = client.embedCreate({
+            title: `About Wall-E`,
+            author: { name: `Wall-E`, url: config.GITHUB, iconURL: config.IMAGE.SQUARE },
+            thumbnail: config.IMAGE.SQUARE,
+            description: `Wall-E serves as the primary server management tool to manage Online College. As personal passion project, Wall-E manages the ticketing system, dynamic voice channels, moderator applications, automoderation & assignment of year & major roles.`,
+            color: config.EMBED_COLORS.BOT_EMBED,
+            footer: { text: `Wall-E`, iconURL: config.IMAGE.SQUARE },
+            timestamp: true,
+        })
+
+        interaction.reply({ embeds: [embed] });
+    }
+};
