3f244b375d4c8daa6a21371885404908ef153a30
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-03-05 17:17:58 -0600

    Rewrite for `guileMemberUpdate` event. `Students` role is now autoassigned

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -3,7 +3,15 @@ module.exports = {
 	name: `guildMemberUpdate`,
 	once: false,
 
-	execute(client, oldMember, newMember) {
-		// TODO: Rewrite
+	async execute(client, oldMember, newMember) {
+		const studentsRoleID = client.config.ROLES.STUDENTS;
+
+		// if the user has the role already, return
+		if (oldMember._roles.includes(studentsRoleID)) return;
+
+		// if the user does not have the students role when adding roles, assign them the role.
+		// Role is assigned late due to community verification requirements
+		let guildMemberObject = oldMember.guild.members.cache.get(oldMember.id);
+		await guildMemberObject.roles.add(studentsRoleID);
 	},
 };
